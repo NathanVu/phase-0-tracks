f5f70b3b11fcea519e3fd5484ecd083042e1e960
Nathan Vu <nathan.vu5@gmail.com>
2016-12-11 23:57:59 -0800

    adding new work to file

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,23 +1,24 @@
-#Encrypt removes the first letter of the string and adds in the letter after the next one. For example abc will become bcd. Must have the same number letters.  
+#Encrypt removes the first letter of the string and adds in the letter after the next one. For example abc will become bcd. Must have the same number of letters.  
 
 
 
 
 def encrypt(strings)
-	index = 0
-	while index < strings.length
-	print strings[index].next
-	index+=1
+	count = 0
+	while count < strings.length #------>abc
+	print strings[count].next
+	count+=1
 	end 
 end
 
 
 
 encrypt("abc")
+encrypt("zed")
 
 
 
-#Decrypt
+#Decrypt reverses the process of encrypt.  Each letter of the string should go backward one letter. 
 
 
 def decrypt(string)
@@ -35,7 +36,32 @@ def decrypt(string)
 	 	end
 	 	string
 	 end
+	 
+	 decrypt("bcd")
+	 decrypt("afe")
+	 
+	 #I beelieve the inside or interior method will run first for encrypt. Eventually it will run decrypt last since it is on the outside. Ultimatelly, it will reverse the encrypt method.
 
+	 
+	 puts "What is your name? Would you like to decrypt or encrypt?"
+	 password = gets.chomp 
+	 
+	 if password == "encrypt" || password == "decrypt"
+	   puts" That's great"
+	   
+	   puts "What is your password?"
+	   password_2 = gets.chomp 
+	   puts "#{password_2}"
+	 end
+	 
+	 
+
+
+
+
+
+
+	 
 
 
 
